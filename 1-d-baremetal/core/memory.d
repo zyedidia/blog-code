18b41faa714d2b61f7b3109dadc3dffc8c7522a1
Zachary Yedidia <zyedidia@gmail.com>
2023-02-13 13:43:23 -0800

    No tree loop distribute patterns

--- 1-d-baremetal/core/memory.d
+++ 1-d-baremetal/core/memory.d
@@ -1,16 +1,21 @@
 module core.memory;
 
-extern (C) size_t strlen(const(char)* s) nothrow {
-    size_t n;
-    for (n = 0; *s != '\0'; ++s) {
-        ++n;
+import gcc.attributes;
+
+@optimize("-fno-tree-loop-distribute-patterns") {
+    extern (C) size_t strlen(const(char)* s) nothrow {
+        size_t n;
+        for (n = 0; *s != '\0'; ++s) {
+            ++n;
+        }
+        return n;
     }
-    return n;
+
+    extern (C) void* memset(void* v, int c, size_t n) nothrow {
+        for (char* p = cast(char*) v; n > 0; ++p, --n) {
+            *p = cast(char) c;
+        }
+        return v;
+    }
 }
 
-extern (C) void* memset(void* v, int c, size_t n) nothrow {
-    for (char* p = cast(char*) v; n > 0; ++p, --n) {
-        *p = cast(char) c;
-    }
-    return v;
-}
